fec5433ccb0fff3a10eba9ce0516ea92ead3b99c
Andrew <llama.army@gmail.com>
2022-05-02 10:38:58 +1000

    Updated for sucessful build

--- flash_linux.sh
+++ flash_linux.sh
@@ -10,16 +10,18 @@
 # DIST=./dist/ttgo_t4_v13
 
 # or m5stack
-DIST=./dist/m5stack_core1
+DIST=./dist/m5stack-core2
 
 # Set USB port
-PORT=/dev/ttyUSB0
+PORT=/dev/ttyACM0
 
-esptool.py -p $PORT -b 921600 write_flash  \
+esptool.py -p $PORT -b 115200 write_flash  \
   --flash_mode dio                         \
   --flash_size detect                      \
   --flash_freq 80m                         \
   0xe000 $DIST/boot_app0.bin               \
   0x1000 $DIST/bootloader_qio_80m.bin      \
-  0x10000 $DIST/evDash.ino.bin             \
+#  0x10000 $DIST/evDash.ino.bin             \
   0x8000 $DIST/evDash.ino.partitions.bin
+  0x10000 $DIST/firmware.bin             \
+#  0x8000 $DIST/partitions.bin
